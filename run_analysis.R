c023ea6da0fabe6195bcef0b02cfe14f24aad202
Xiao <qxshower@hotmail.com>
2021-01-13 21:42:53 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -45,7 +45,7 @@ names(TidyData)<-gsub("Mag", "Magnitude", names(TidyData))
 names(TidyData)<-gsub("tBody", "TimeBody", names(TidyData))
 names(TidyData)<-gsub("-mean\\(\\)", "Mean", names(TidyData), ignore.case = TRUE)
 names(TidyData)<-gsub("-std\\(\\)", "STD", names(TidyData), ignore.case = TRUE)
-names(TidyData)<-gsub("-freq\\(\\)", "Frequency", names(TidyData), ignore.case = TRUE)
+names(TidyData)<-gsub("-meanfreq\\(\\)", "Frequency", names(TidyData), ignore.case = TRUE)
 names(TidyData)<-gsub("angle", "Angle", names(TidyData))
 names(TidyData)<-gsub("gravity", "Gravity", names(TidyData))
 
